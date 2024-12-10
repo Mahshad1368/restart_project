7f7bade6d5d4f42d6c11d2d57d81c38519fc67c3
Mahshad <mahshad1368@gmail.com>
2024-12-10 10:20:57 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mahshad Jafari on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
