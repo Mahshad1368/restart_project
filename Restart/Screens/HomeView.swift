8694b1d78c24a04c939bf9a518f3454afe4dab47
Mahshad <mahshad1368@gmail.com>
2024-12-12 12:36:55 +0100

    Animation part1

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     //MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
     
     //MARK: - BODY
     
@@ -24,6 +25,13 @@ struct HomeView: View {
                     .resizable()
                     .scaledToFit()
                     .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation (
+                        Animation
+                            .easeOut(duration: 4)
+                            .repeatForever()
+                        , value: isAnimating
+                    )
             }
             
             
@@ -34,13 +42,16 @@ struct HomeView: View {
                 .fontWeight(.light)
                 .foregroundColor(.secondary)
                 .multilineTextAlignment(.center)
+                .padding()
             
             
             //MARK: - FOOTER
             Spacer()
             
             Button(action: {
-                isOnboardingViewActive = true
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
             }) {
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
@@ -53,6 +64,12 @@ struct HomeView: View {
             .buttonBorderShape(.capsule)
             .controlSize(.large)
         } //VSTACK
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                isAnimating = true
+            }
+            
+        }
     }
 }
 
--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,6 +12,7 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
+    @State private var isAnimating: Bool = false
 
     //MARK: - BODY
     
@@ -42,12 +43,19 @@ struct OnboardingView: View {
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 10)
                 } // :HEADER
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                
             //MARK: - CENTER
                 ZStack {
                     CircleGroup(shapeColor:.white, shapeOpacity: 0.2)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
                     
                 }//: CENTER
                 
@@ -106,25 +114,31 @@ struct OnboardingView: View {
                                     
                                 }
                                 .onEnded { _ in
-                                    if buttonOffset > buttonWidth / 2 {
-                                        buttonOffset = buttonWidth - 80
-                                        isOnboardingViewActive = false
-                                    }else {
-                                        buttonOffset = 0
-                                        
+                                    withAnimation(Animation.easeOut(duration: 0.4)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        }else {
+                                            buttonOffset = 0
+                                        }
                                     }
-                                    
-                                    
                                 }
                             )
                         Spacer()
                     } //: HSTACK
+                   
  
                 }//:FOOTER
                 .frame(width: buttonWidth,height: 80, alignment: .center)
                 .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
             }//: VSTACK
         } //ZSTACK
+        .onAppear {
+            isAnimating = true
+        }
     }
 }
 
